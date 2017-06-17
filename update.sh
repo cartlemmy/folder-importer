f148995c0ed862b3614b3dcebc92bb0aa24dc9d5
Josh <josh@josh-desktop>
2017-06-17 16:42:45 -0700

    * fixed start.sh to work properly

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-wget https://github.com/cartlemmy/folder-importer/archive/master.zip
+curl -LOk https://github.com/cartlemmy/folder-importer/archive/master.zip -o master.zip
 unzip master.zip
 rsync -arv folder-importer-master/ folder-importer
 rm master.zip
