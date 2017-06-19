791cb36bbe0d3c752faccad8d45cd10b7b18a936
Josh <josh@josh-desktop>
2017-06-19 08:16:56 -0700

    Don't spawn multiple fi-sync

--- upload-check.sh
+++ upload-check.sh
@@ -9,14 +9,11 @@ source cmd-rsync > /tmp/upload-files-state
 	
 #rsync -e "ssh -i $HOME/.ssh/media${USER}_rsa.pub" -arv  --include-from rsync-include --exclude-from rsync-exclude --delete "$UPLOAD_FROM_DIR" "paliportal@paliportal.com:/home/paliportal/public_html/data/folder-import/queue/$USER/" | grep -v 'bytes/sec' | grep -v 'speedup' > /tmp/upload-files-state
 
-if hash pidof 2>/dev/null; then
-	if ! pidof -x "fi-sync.sh" >/dev/null; then
-		nohup ./fi-sync.sh "$UPLOAD_FROM_DIR" >./fi-sync.log.txt 2>&1 &
-	fi
+if ps aux | grep "upload-check-loop.sh" | grep -vq "grep"; then
+	echo "fi-sync.sh running"
 else
-	if ! pgrep -xq -- "fi-sync.sh"; then
-		nohup ./fi-sync.sh "$UPLOAD_FROM_DIR" >./fi-sync.log.txt 2>&1 &
-	fi
+	echo "Spawning fi-sync.sh, sync from $UPLOAD_FROM_DIR"
+	nohup ./fi-sync.sh "$UPLOAD_FROM_DIR" >./fi-sync.log.txt 2>&1 &
 fi
 
 	printf "$USER\n$(/sbin/ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')\n\ndiff:" > /tmp/upload-files-diff
