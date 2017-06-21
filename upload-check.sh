51595a3fa87f70023dcac7a278933ecc7da94fc1
Josh <josh@josh-desktop>
2017-06-20 17:29:06 -0700

    Fiiixxxx

--- upload-check.sh
+++ upload-check.sh
@@ -9,7 +9,7 @@ source cmd-rsync > /tmp/upload-files-state
 	
 #rsync -e "ssh -i $HOME/.ssh/media${USER}_rsa.pub" -arv  --include-from rsync-include --exclude-from rsync-exclude --delete "$UPLOAD_FROM_DIR" "paliportal@paliportal.com:/home/paliportal/public_html/data/folder-import/queue/$USER/" | grep -v 'bytes/sec' | grep -v 'speedup' > /tmp/upload-files-state
 
-if ps aux | grep "upload-check-loop.sh" | grep -vq "grep"; then
+if ps aux | grep "fi-sync.sh" | grep -vq "grep"; then
 	echo "fi-sync.sh running"
 else
 	echo "Spawning fi-sync.sh, sync from $UPLOAD_FROM_DIR"
@@ -25,9 +25,9 @@ else
 	cat /tmp/upload-files-state >> /tmp/upload-files-diff
 	printf "cd \"${UPLOAD_FROM_DIR}\"\n" > fi-cmds
 	curl -sS --data-binary "@/tmp/upload-files-diff" https://paliportal.com/fi?a=status >> fi-cmds
-	cat fi-cmds
-	chmod +x fi-cmds
-	./fi-cmds
+	#cat fi-cmds
+	#chmod +x fi-cmds
+	#./fi-cmds
 
 fi
 
