1fff0e32abcb5000e0b6661bc117482fde83f992
Josh <josh@josh-desktop>
2017-06-20 17:04:38 -0700

    Mo fixes

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 if ps aux | grep "upload-check-loop.sh" | grep -vq "grep"; then
-	echo "running"
-	exit
+	if [ $1 = "force" ]; then
+		echo "forcing restart"
+		pkill bash
+		pkill rsync
+	else 
+		echo "running"
+		exit
+	fi
 fi
+
+echo "" > ./log.txt
+echo "" > ./fi-sync.log.txt
 
 echo "Starting folder importer"
 cd ~/folder-importer; nohup ./upload-check-loop.sh > ./log.txt 2>&1 & echo $! > ./upload-check-loop.sh.pid
