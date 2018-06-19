b3639201e8508037460c934cd19c82fcbc35a789
cartlemmy <cartlemmy@gmail.com>
2018-06-19 15:32:59 -0700

    Not needed, updating from git

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~
-curl -LOk https://github.com/cartlemmy/folder-importer/archive/master.zip -o master.zip
-unzip master.zip
-rsync -arv folder-importer-master/ folder-importer
-rm master.zip
-rm -rf master.zip folder-importer-master
-cd ~/folder-importer
