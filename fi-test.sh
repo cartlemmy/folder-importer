ad1c7f83766407b5d4f08134e83b73d4f43c13b3
cartlemmy <cartlemmy@gmail.com>
2018-06-26 15:35:58 -0700

    Update fi-test.sh

--- fi-test.sh
+++ fi-test.sh
@@ -2,6 +2,19 @@
 
 source importer.cfg
 
-OPTS="--dry-run"
+echo "UPLOAD_FROM_DIR=$UPLOAD_FROM_DIR"
+echo "FROM_SUBFOLDER=$FROM_SUBFOLDER"
+echo "TO_SUBFOLDER=$TO_SUBFOLDER"
 
-source cmd-rsync
+OPTS="--dry-run -v"
+
+set -x
+
+cd "$UPLOAD_FROM_DIR/$FROM_SUBFOLDER"
+
+rsync -e "ssh -i $HOME/.ssh/media${USER}_rsa.pub" \
+        -ri --ignore-existing --prune-empty-dirs \
+        --filter="merge $HOME/folder-importer/fi-filter" \
+        $OPTS \
+        . \
+        "paliportal@paliportal.com:/home/paliportal/public_html/data/folder-import/queue/$USER/$TO_SUBFOLDER"
