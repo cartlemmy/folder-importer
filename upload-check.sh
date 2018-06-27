553d215b06312602611dc54cdaf4d0d219981f78
cartlemmy <cartlemmy@gmail.com>
2018-06-26 20:40:14 -0700

    Update upload-check.sh

--- upload-check.sh
+++ upload-check.sh
@@ -2,10 +2,12 @@
 
 source importer.cfg
 
-mv /tmp/upload-files-state /tmp/upload-files-state.prev
+TMP_PATH="$(pwd)/tmp"
+mv "$TMP_PATH/upload-files-state" "$TMP_PATH/upload-files-state.prev"
 
 OPTS="--dry-run"
-source cmd-rsync > /tmp/upload-files-state
+
+source cmd-rsync > "$TMP_PATH/upload-files-state"
 	
 #rsync -e "ssh -i $HOME/.ssh/media${USER}_rsa.pub" -arv  --include-from rsync-include --exclude-from rsync-exclude --delete "$UPLOAD_FROM_DIR" "paliportal@paliportal.com:/home/paliportal/public_html/data/folder-import/queue/$USER/" | grep -v 'bytes/sec' | grep -v 'speedup' > /tmp/upload-files-state
 
@@ -16,15 +18,15 @@ else
 	nohup ./fi-sync.sh "$UPLOAD_FROM_DIR" >./fi-sync.log.txt 2>&1 &
 fi
 
-	printf "$USER\n$(/sbin/ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')\n\ndiff:" > /tmp/upload-files-diff
-if cmp -s /tmp/upload-files-state /tmp/upload-files-state.prev
+	printf "$USER\n$(/sbin/ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')\n\ndiff:" > "$TMP_PATH/upload-files-diff"
+if cmp -s "$TMP_PATH/upload-files-state" "$TMP_PATH/upload-files-state.prev"
 then
-	curl -sS --data-binary "@/tmp/upload-files-diff" https://paliportal.com/fi?a=heartbeat
+	curl -sS --data-binary "@$TMP_PATH/upload-files-state" https://paliportal.com/fi?a=heartbeat
 	exit
 else
-	cat /tmp/upload-files-state >> /tmp/upload-files-diff
-	printf "cd \"${UPLOAD_FROM_DIR}\"\n" > fi-cmds
-	curl -sS --data-binary "@/tmp/upload-files-diff" https://paliportal.com/fi?a=status >> fi-cmds
+	#cat "$TMP_PATH/upload-files-state" >> "$TMP_PATH/upload-files-diff"
+	#printf "cd \"${UPLOAD_FROM_DIR}\"\n" > fi-cmds
+	curl -sS --data-binary "@$TMP_PATH/upload-files-state" https://paliportal.com/fi?a=status
 	#cat fi-cmds
 	#chmod +x fi-cmds
 	#./fi-cmds
