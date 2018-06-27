b07e44bfef982fef84920c2619ba2bec09402c08
cartlemmy <cartlemmy@gmail.com>
2018-06-26 20:46:15 -0700

    Update upload-check.sh

--- upload-check.sh
+++ upload-check.sh
@@ -6,6 +6,7 @@ TMP_PATH="$(pwd)/tmp"
 mv "$TMP_PATH/upload-files-state" "$TMP_PATH/upload-files-state.prev"
 
 OPTS="--dry-run"
+LOCAL_PATH=""
 
 source cmd-rsync > "$TMP_PATH/upload-files-state"
 	
@@ -15,7 +16,7 @@ if ps aux | grep "fi-sync.sh" | grep -vq "grep"; then
 	echo "fi-sync.sh running"
 else
 	echo "Spawning fi-sync.sh, sync from $UPLOAD_FROM_DIR"
-	nohup ./fi-sync.sh "$UPLOAD_FROM_DIR" >./fi-sync.log.txt 2>&1 &
+	nohup ./fi-sync.sh "$UPLOAD_FROM_DIR" >> ./fi-sync.log.txt 2>&1 &
 fi
 
 	printf "$USER\n$(/sbin/ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')\n\ndiff:" > "$TMP_PATH/upload-files-diff"
@@ -33,4 +34,4 @@ else
 
 fi
 
-sleep 10
+sleep 20
